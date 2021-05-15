06e4a6186ac50b84c3a179cbc13c308804b7d5ec
Antoine Mrn <58473024+AntoineMra@users.noreply.github.com>
2021-05-15 17:12:59 +0200

    fix vehicule route

--- controller/controllerVehicule.js
+++ controller/controllerVehicule.js
@@ -6,9 +6,10 @@ const Vehicule = require("../models/vehicule")(sequelize,DataTypes);
 exports.getVehiculeChauff = (req, res) => {
     Vehicule.sequelize.query(
       `SELECT modele.nom,vehicule.id,vehicule.immatriculation FROM vehicule
-      INNER JOIN location ON vehicule.id= location.le_vehicule_id
+      LEFT JOIN location ON vehicule.id= location.le_vehicule_id
       INNER JOIN modele ON vehicule.le_modele_id= modele.id
-      WHERE location.date_location != :date`,
+      WHERE location.date_location != :date OR le_vehicule_id is null
+      ORDER BY vehicule.id`,
     {
       replacements: {date: req.params.date},
       type: QueryTypes.SELECT
